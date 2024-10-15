f8985ace62486f40a1c0bb80d60c7c4cbc8c5512
emagstrein <emagstrein@tsn.at>
2024-10-15 11:22:44 +0200

    Verändern des Algorithmus in der Main

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         LocalDate flugdatum = LocalDate.of(2024, 1, 20);
-        Angebot angebot;
+        Rabattstrategie rabattstrategie;
 
         if (flugdatum.getMonth().getValue() == 1 || flugdatum.getMonth().getValue() == 4 || flugdatum.getMonth().getValue() == 10){
-            angebot = new MaxiDiscount(100, flugdatum, "1", "Cooler Flug");
+            rabattstrategie = new MaxiDiscount(100, flugdatum, "1", "Maximaler Rabatt");
+        } else if (flugdatum.getMonth().getValue() == 2 || flugdatum.getMonth().getValue() == 3) {
+            rabattstrategie = new MidiDiscount(100, flugdatum, "2", "Mittlerer Rabatt");
+        } else {
+            rabattstrategie = new ZeroDiscount(100, flugdatum, "3", "Kein Rabatt");
         }
+        System.out.println(rabattstrategie);
+        System.out.println(rabattstrategie.getReduzierterPreis(rabattstrategie.getRegulaerpreis()));
     }
 }
